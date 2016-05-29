74a6086a6fb7bdba61ec003902d21068ccf80988
Mayank Pratap <mayank7860@gmail.com>
2016-05-29 12:10:15 +0530

    JSON Handling Is Done

--- js/wiki.js
+++ js/wiki.js
@@ -2,21 +2,56 @@
 
   // Initialize search variable
 
+    var searchTerm='';
+
     function search(){
 
-       searchTerm="india";
+       console.log(searchTerm);
+
        var url="https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch="+searchTerm+"&gsrlimit=10&prop=extracts&exintro&explaintext&exsentences=1&exlimit=max";
 
        $.getJSON(url,function(json){
 
-           console.log(JSON.stringify(json));
+           handleMyJSON(json);
 
        });
 
+       function handleMyJSON(json){
+
+          var temp=json.query.pages;
+          for(i in temp){
+
+             var pageId=temp[i].pageid;
+             var title=temp[i].title;
+             var extract=temp[i].extract;
+
+             var link="http://en.wikipedia.org/wiki/?curid="+pageId;
+             var html="<div class='well'><a href="+link+" target='_blank'><h2>"+title+"</h2><p>"+extract+"</p></a></div>";
+             $('.container').append(html);
+          }
+
+       }
 
     }
 
+    $('#search').keypress(function(e){
+
+         if(e.which==13){
+
+            console.log("Hello");
+
+            searchTerm=$('#search').val();
+            // Reset container
+            $('.container').empty();
+
+            search();
+         }
+
+    });
+
+
 });
+
 
 /*$(document).ready(function(){
 
